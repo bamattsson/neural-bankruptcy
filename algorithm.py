d65bfbb45d6f68cd9922a043406e79592c424b76
Björn Mattsson <bamattsson@gmail.com>
2017-04-02 22:27:30 +0100

    adding abstract algorithm class

---
+++ algorithm.py
@@ -0,0 +1,36 @@
+from abc import ABCMeta
+import numpy as np
+
+
+class Algorithm(metaclass=ABCMeta):
+    """Abstract algorithm class."""
+
+    @abstractmethod
+    def fit(self, samples, labels):
+        """
+        Args:
+            samples (np.ndarray): X data, shape (n_samples, n_features)
+            labels (np.ndarray): y data, shape (n_samples)
+        """
+        pass
+
+    @abstractmethod
+    def predict_proba(self, samples):
+        """
+        Args:
+            samples (np.ndarray): X data, shape (n_samples, n_features)
+        Returns:
+            proba (np.ndarray): Probability of belonging to a particular class, shape (n_samples, n_classes)
+        """
+        proba = np.zeros(samples.shape[0])
+        return proba
+
+    def predict(self, samples):
+        """
+        Args:
+            samples (np.ndarray): X data, shape (n_samples, n_features)
+        Returns:
+            predict (np.ndarray): Predicted class, shape (n_samples)
+        """
+        predict_proba = self.predict_proba(samples)
+        predict = np.argmax(predict_proba, axis=1)
